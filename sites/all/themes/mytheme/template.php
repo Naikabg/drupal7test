7e74c049101fa44be5ff758928c16c31ec68f1cb
Naikabg <naika.bg@gmail.com>
2018-02-20 19:06:31 +0200

    Working with Display Suite

--- sites/all/themes/mytheme/template.php
+++ sites/all/themes/mytheme/template.php
@@ -6,3 +6,10 @@ function mytheme_css_alter(&$css) {
   // Remove Drupal core CSS.
   unset($css[drupal_get_path('module', 'node') . '/node.css']);
 }
+
+function mytheme_preprocess_node(&$vars) {
+  if($vars['view_mode'] == 'teaser') {
+    $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->type . '__teaser';
+    $vars['theme_hook_suggestions'][] = 'node__' . $vars['node']->nid . '__teaser';
+  }
+}
--- sites/all/themes/mytheme/templates/node.tpl.php
+++ sites/all/themes/mytheme/templates/node.tpl.php
@@ -100,7 +100,10 @@
       // We hide the comments and links now so that we can render them later.
       hide($content['comments']);
       hide($content['links']);
-      print render($content);
+
+      print render($content['	field_tags']);
+      print render($content['body']);
+      print render($content['field_basicpage_image']);
     ?>
   </div>
 
